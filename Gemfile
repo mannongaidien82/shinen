de189076e584eecdfe67d10e97ca6a7cbcb3a3a7
Ilya Smelkov <ismelkov@restream.rt.ru>
2016-07-12 17:25:14 +0300

    Minify json resources

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem 'rake'
 
 gem 'slim'
 gem 'sass'
+gem 'json-minify'
--- config.rb
+++ config.rb
@@ -1,44 +1,46 @@
-###
-# Page options, layouts, aliases and proxies
-###
+# require "middleman-core"
 
-# Per-page layout changes:
-#
-# With no layout
+class MinifyJsonExtension < ::Middleman::Extension
+  def initialize( app, options_hash={}, &block )
+    super
+    require 'json'
+    require 'json/minify'
+  end
+
+  def ready
+    app.use Rack
+  end
+
+  class Rack
+    def initialize(app, options={})
+      @app = app
+    end
+
+    def call(env)
+      status, headers, response = @app.call(env)
+      if 'application/json' == headers[ 'Content-Type' ]
+        minified = JSON.minify ::Middleman::Util.extract_response_text( response )
+        headers['Content-Length'] = ::Rack::Utils.bytesize(minified).to_s
+        response = [minified]
+      end
+      return status, headers, response
+    end
+  end
+end
+
+Middleman::Extensions.register( :minify_json ) { MinifyJsonExtension }
+
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
+  activate :relative_assets
+  activate :minify_json
   activate :minify_css
-
-  # Minify Javascript on build
   # activate :minify_javascript
 end
